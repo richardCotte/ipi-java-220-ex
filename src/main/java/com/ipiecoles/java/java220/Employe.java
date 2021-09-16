a2bb2a7418819b6c3d5d86d4cde4f583b67dce76
richardCotte <rcotte1582@gmail.com>
2021-09-16 11:42:50 +0200

    101, 102, 103

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -36,6 +36,8 @@ public class Employe {
         return anneeAnciennete;
     }
 
+    public Integer getNbConges() { return Entreprise.NB_CONGES_BASE; }
+
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
 
@@ -46,7 +48,15 @@ public class Employe {
     public void setMatricule(String matricule) { this.matricule = matricule; }
 
     public LocalDate getDateEmbauche() { return dateEmbauche; }
-    public void setDateEmbauche(LocalDate dateEmbauche) { this.dateEmbauche = dateEmbauche; }
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception {
+        if(dateEmbauche == null){
+            throw new Exception("La date d'embauche ne peut pas être nulle");
+        }
+        if(dateEmbauche.isAfter(LocalDate.now())){
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
+        }
+            this.dateEmbauche = dateEmbauche;
+    }
 
     public Double getSalaire() { return salaire; }
     public void setSalaire(Double salaire) { this.salaire = salaire; }
