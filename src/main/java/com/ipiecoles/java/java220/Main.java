1799182c5c60c787785cf00bdd12bc8f526d12c0
richardCotte <rcotte1582@gmail.com>
2021-09-17 15:21:49 +0200

    Évaluation

--- src/main/java/com/ipiecoles/java/java220/Main.java
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -4,6 +4,5 @@ import org.joda.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Employe employe1 = new Employe("Doe", "John", "M12345", new LocalDate(2015, 10, 31), 2000.0);
     }
 }
